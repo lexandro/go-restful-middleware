44f596b933159167f356dc876e8adf0f765b53f2
lexandro <lexandro2000@gmail.com>
2016-07-22 00:52:44 +0200

    Finishing first iteration on Logger middleware modul

--- logger.go
+++ logger.go
@@ -4,48 +4,57 @@ package middleware
 
 import (
 	"github.com/emicklei/go-restful"
-	"github.com/emicklei/go-restful/log"
 	"net/http"
 	"time"
+	"github.com/emicklei/go-restful/log"
 )
 
-// https://httpd.apache.org/docs/2.2/logs.html#combined + execution time.
-const apacheFormatPattern = "%s - - [%s] \"%s %s %s\" %d %d \"%s\" \"%s\" %.4f\n"
+// Based on: https://httpd.apache.org/docs/2.4/logs.html#accesslog
+const apacheFormatPattern = "%s - - [%s] \"%s %s %s\" %d %d \"%s\" \"%s\"\n"
 
 type ApacheLogRecord struct {
-	http.ResponseWriter
-
 	ip                    string
-	time                  time.Time
+	time                  string
 	method, uri, protocol string
 	status                int
-	responseBytes         int64
+	responseBytes         int
 	referer               string
 	userAgent             string
-	elapsedTime           time.Duration
 }
 
 func ApiLogger(req *restful.Request, resp *restful.Response, chain *restful.FilterChain) {
-	startTime := time.Now()
 	//
 	chain.ProcessFilter(req, resp)
+	r := req.Request
+	lr := createLogRecordFrom(r, resp)
+	log.Logger.Printf(apacheFormatPattern, lr.ip, lr.time, lr.method, lr.uri, lr.protocol, lr.status, lr.responseBytes, lr.referer, lr.userAgent)
+}
+
+func createLogRecordFrom(req *http.Request, resp *restful.Response) (result *ApacheLogRecord) {
+	//127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)"
 	//
 	finishTime := time.Now()
-	timeFormatted := finishTime.UTC().Format("02/Jan/2006 03:04:05")
+	timeFormatted := finishTime.UTC().Format("02/Jan/2006 03:04:05 -0700")
 	//
-
-	r := req.Request
-
-	referer := r.Referer()
-	if referer == "" {
-		referer = "-"
+	result = &ApacheLogRecord{
+		ip:             nvl(req.RemoteAddr),
+		time:          timeFormatted,
+		method:        nvl(req.Method),
+		uri:           nvl(req.URL.Path),
+		protocol:      nvl(req.Proto),
+		status:        resp.StatusCode(),
+		responseBytes: resp.ContentLength(),
+		referer:       nvl(req.Referer()),
+		userAgent:     nvl(req.UserAgent()),
 	}
 
-	userAgent := r.UserAgent()
-	if userAgent == "" {
-		userAgent = "-"
+	return result
+}
+
+func nvl(content string) string {
+	if len(content) > 0 {
+		return content
+	} else {
+		return "-"
 	}
-
-	log.Logger.Printf(apacheFormatPattern, r.RemoteAddr, timeFormatted, r.Method, r.RequestURI, r.Proto, resp.StatusCode(), resp.ContentLength(), referer, userAgent, finishTime.Sub(startTime).Seconds())
-
 }
--- logger_test.go
+++ logger_test.go
@@ -1,40 +1,51 @@
 package middleware
 
 import (
-	"testing"
 	"github.com/emicklei/go-restful"
-	"net/http/httptest"
+	"github.com/lexandro/go-assert"
 	"net/http"
+	"testing"
 	"fmt"
 )
 
 type FakeLogger struct {
-
+	Calls     int
+	LastEntry string
 }
 
 func Test_ApiLogger(t *testing.T) {
-	resp := httptest.NewRecorder()
+	// given
 	req, err := http.NewRequest("GET", "/test/logger", nil)
+	req.RemoteAddr = "127.456.789.012"
+	req.Header.Add("User-Agent", "fakeAgent")
+	req.Header.Add("Referer", "fakeReferer")
 	if err != nil {
 		return
 	}
 	ws := new(restful.WebService)
-	ws.Path("/test").Route(ws.GET("/logger").To(loggerFunc))
+	ws.Path("/test").Route(ws.GET("/logger").To(dummyHandleFunc))
 	restful.Filter(ApiLogger)
 	restful.Add(ws)
-	restful.SetLogger(&FakeLogger{})
-	//
-	SendRequest(req, resp)
+	fl := &FakeLogger{
+	}
+	restful.SetLogger(fl)
+	// when
+	SendRequest(req, nil)
+	// then
+	assert.Equals(t, 1, fl.Calls)
+	assert.Equals(t, "127.456.789.012 - - [21/Jul/2016 10:49:32 +0000] \"GET /test/logger HTTP/1.1\" 200 0 \"fakeReferer\" \"fakeAgent\"\n", fl.LastEntry)
 
 }
 
 func (fl *FakeLogger) Print(v ...interface{}) {
-	fmt.Println("1")
+	fl.Calls++
 
 }
 func (fl *FakeLogger) Printf(format string, v ...interface{}) {
-	fmt.Println("2")
+	fl.Calls++
+	v[1] = "21/Jul/2016 10:49:32 +0000" // hardcoding time
+	fl.LastEntry = fmt.Sprintf(format, v...)
 }
 
-func loggerFunc(_ *restful.Request, _ *restful.Response) {
+func dummyHandleFunc(_ *restful.Request, _ *restful.Response) {
 }
